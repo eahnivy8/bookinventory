471502c37f7c5ba2ea5ab9ebd8db5126ef97c869
Eddie <eahnivy8@gmail.com>
2019-09-09 14:01:07 +0900

    Initial Commit

---
+++ VisualBookManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisualBookManager
+//
+//  Created by Eddie Ahn on 09/09/2019.
+//  Copyright © 2019 Eddie Ahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
